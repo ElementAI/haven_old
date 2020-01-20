d799a3059aa42a6fd517481507271ddeb406fbc9
IssamLaradji <issam.laradji@gmail.com>
2020-01-20 03:31:09 +0000

    results

--- haven/haven_results.py
+++ haven/haven_results.py
@@ -260,13 +260,15 @@ def get_dataframe_score_list(exp_list, col_list=None, savedir_base=None):
 
 def get_images(exp_list, savedir_base, n_exps=3, n_images=1, split="row",
                height=12, width=12, legend_list=None):
+    assert(legend_list is not None)
     for k, exp_dict in enumerate(exp_list):
         if k >= n_exps:
             return
         result_dict = {}
-
+ 
         label = "_".join([str(exp_dict.get(k)) for 
                                 k in legend_list])
+        
 
         exp_id = hu.hash_dict(exp_dict)
         result_dict["exp_id"] = exp_id
@@ -303,61 +305,6 @@ def get_images(exp_list, savedir_base, n_exps=3, n_images=1, split="row",
         
         plt.show()
 
-
-def upload_score_list_to_dropbox(exp_id_list, savedir_base, outdir_base, access_token):
-    import dropbox
-    dbx = dropbox.Dropbox(access_token)
-      
-    try:
-        dbx.files_create_folder(outdir_base)
-    except:
-        pass
-    # API v2
-    for exp_id in exp_id_list:
-        savedir = savedir_base + "/%s" % exp_id
-        outdir = outdir_base + "/%s" % exp_id
-
-        try:
-            dbx.files_create_folder(outdir)
-        except:
-            pass
-
-        with open(savedir+"/exp_dict.json", 'rb') as f:
-            dbx.files_upload(f.read(), outdir+"/exp_dict.json")
-
-        with open(savedir+"/score_list.pkl", 'rb') as f:
-            dbx.files_upload(f.read(), outdir+"/score_list.pkl")
-
-        print('saved: https://www.dropbox.com/home/%s' % outdir)
-
-    
-def upload_file_to_dropbox(src_fname, out_fname, access_token):
-    import dropbox
-    dbx = dropbox.Dropbox(access_token)
-    try:
-        dbx.files_delete_v2(out_fname)
-    except:
-        pass
-    with open(src_fname, 'rb') as f:
-        dbx.files_upload(f.read(), out_fname)
-
-def zipdir(exp_id_list, savedir_base, src_fname):
-    import zipfile
-    zipf = zipfile.ZipFile(src_fname, 'w', zipfile.ZIP_DEFLATED)
-
-    # ziph is zipfile handle
-    for exp_id in exp_id_list:
-        if not os.path.isdir(os.path.join(savedir_base, exp_id)):
-            continue
-        abs_path = "%s/%s/exp_dict.json" % (savedir_base, exp_id)
-        rel_path = "%s/exp_dict.json" % exp_id
-        zipf.write(abs_path, rel_path)
-
-        abs_path = "%s/%s/score_list.pkl" % (savedir_base, exp_id)
-        rel_path = "%s/score_list.pkl" % exp_id
-        zipf.write(abs_path, rel_path)
-
-    zipf.close()
 
 
 def group_exp_list(exp_list, groupby_key_list):
