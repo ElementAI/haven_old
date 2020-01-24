84e78a7e5b8a681502938fff8770951b2bd28918
IssamLaradji <issam.laradji@gmail.com>
2020-01-24 17:32:33 +0000

    added installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='haven',
+      version='0.1',
+      description='Manage large-scale experiments',
+      url='https://github.com/ElementAI/haven_borgy',
+      author='Issam Laradji',
+      author_email='issam.laradji@elementai.com',
+      license='MIT',
+      packages=['haven'],
+      zip_safe=False)
