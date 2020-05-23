9eaa9cafa0b89c08f179e6521e9e48cdfbce3368
IssamLaradji <issam.laradji@gmail.com>
2020-05-23 18:11:23 +0000

    added ork2

--- haven/haven_orkestrator.py
+++ haven/haven_orkestrator.py
@@ -95,8 +95,11 @@ def get_job(api, job_id):
         raise ValueError("job id %s not found." % job_id)
 
 def get_jobs(api, **kwargs):
+    account_id = kwargs.get('account_id')
+    if account_id is None:
+        account_id = hu.subprocess_call('eai account get').split('\n')[-2].split(' ')[0]
     return api.v1_account_job_get(
-            account_id=kwargs['account_id'],
+            account_id=account_id,
             limit=1000, 
             order='-created',
             q="alive_recently=True").items
